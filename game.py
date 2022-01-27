f82c87e9f3e7f9ef621c566727a62d1716778fd9
afv14 <97483119+afv14@users.noreply.github.com>
2022-01-26 21:50:25 -0600

    Added Data Validation - need to loop code

--- game.py
+++ game.py
@@ -11,11 +11,30 @@ user_choice = input("Let's get started! Please choose one of the following:  'ro
 print("You chose", user_choice)
 
 
-
-
-
 # validate inputs
-
+if user_choice == "Rock":
+    user_choice = "rock"
+elif user_choice == "ROCK":
+    user_choice = "rock"
+elif user_choice == "rock":
+    user_choice = "rock"
+elif user_choice == "Paper":
+    user_choice = "paper"
+elif user_choice == "PAPER":
+    user_choice = "paper"
+elif user_choice == "paper":
+    user_choice = "paper"
+elif user_choice == "Scissors":
+    user_choice = "scissors"
+elif user_choice == "SCISSORS":
+    user_choice = "scissors"
+elif user_choice == "scissors":
+    user_choice = "scissors"
+else:
+    print("Please choose one of the following selections: 'rock', 'paper', 'scissors'!")
+    quit()
+#I have to get it to loop around so that it keeps asking the user for an input
+#not done with this section
 
 
 # computer choice
