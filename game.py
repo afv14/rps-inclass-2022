e2b43e09a7c33f54602933b6668ce7e92150540b
afv14 <97483119+afv14@users.noreply.github.com>
2022-02-03 13:51:50 -0500

    Updated Randomization and message boxes

--- game.py
+++ game.py
@@ -8,7 +8,6 @@
 # ask for use inputs
 user_choice = input("Let's get started! Please choose one of the following:  'rock', 'paper', 'scissors': ")
 
-print("You chose", user_choice)
 
 
 # validate inputs
@@ -17,15 +16,21 @@ if user_choice == "Rock":
     user_choice = "rock"
 elif user_choice == "ROCK":
     user_choice = "rock"
+elif user_choice == "rock":
+    user_choice = "rock"
 #switch capitalization of paper
 elif user_choice == "Paper":
     user_choice = "paper"
 elif user_choice == "PAPER":
     user_choice = "paper"
+elif user_choice == "paper":
+    user_choice = "paper"
 #switch capitalization of scissors
 elif user_choice == "Scissors":
     user_choice = "scissors"
 elif user_choice == "SCISSORS":
+    user_choice = "scissors"
+elif user_choice == "scissors":
     user_choice = "scissors"
 #if input invalid send a message and quit the program
 #I want it to look back to the input at the beginning
@@ -33,14 +38,18 @@ else:
     print("You have entered an invalid input. Please make sure you enter one of the following selections: 'rock', 'paper', 'scissors'!")
     quit()
 
-print(user_choice)
+print("You chose", user_choice)
 
 # computer choice
+#import random
+import random
+
+#create a list
 computer_options = ["rock","paper","scissors"]
-computer_choice = random(computer_options)
-print(computer_choice)
 
-
+#randomize selection
+computer_choice = random.choice(computer_options)
+print("The computer chose", computer_choice)
 
 
 #determine the winner
